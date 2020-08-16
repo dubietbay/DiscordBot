db87a9530d1befa11443b84b99b0b3a818fde2c3
dubietbay <fdadwd@gmail.com>
2020-08-16 14:05:06 +0700

    w

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '?';
 const fs = require('fs');
-client.command = new Discord.Collection();
+client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
