7350f5b1f6374d79706f1ccda2bc2b2e70eabc81
dubietbay <fdadwd@gmail.com>
2020-08-18 12:52:44 +0700

    a

--- commands/help.js
+++ commands/help.js
@@ -9,6 +9,7 @@ module.exports = {
         embed.setAuthor('made by Dub', 'https://i.imgur.com/Rn9muMO.png', 'https://www.roblox.com/users/93839005/profile');
         embed.setThumbnail('https://t1.rbxcdn.com/1194a83cefa36aae9055f96b0165858e');
         embed.addField('?raid','give raids info');
+        embed.addField('?hheeggeell','pls dont use');
         message.channel.send(embed);
     }
 }
