478ffb05d4ec393ffbe7aff4f534310137f1166d
dubietbay <fdadwd@gmail.com>
2020-08-22 10:17:41 +0700

    E

--- main.js
+++ main.js
@@ -49,7 +49,16 @@ client.on('message', message => {
     }
 
     else if (command === 'search') {
-        
+        let invite = await message.channel.createInvite(
+            {
+              maxAge: 10 * 60 * 1000, // maximum time for the invite, in milliseconds
+              maxUses: 1 // maximum times it can be used
+            },
+            `Requested with command by ${message.author.tag}`
+          )
+          .catch(console.log);
+          
+            message.reply(invite ? `Here's your invite: ${invite}` : "There has been an error during the creation of the invite."); 
     }
 });
 
