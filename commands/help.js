4cb7d4f46810a71e325860df84a985b5139efb5e
dubietbay <fdadwd@gmail.com>
2020-08-23 22:37:43 +0700

    E

--- commands/help.js
+++ commands/help.js
@@ -8,9 +8,9 @@ module.exports = {
         embed.setColor('#f4c871');
         embed.setAuthor('made by Dub', 'https://i.imgur.com/Rn9muMO.png', 'https://www.roblox.com/users/93839005/profile');
         embed.setThumbnail('https://t1.rbxcdn.com/1194a83cefa36aae9055f96b0165858e');
-        embed.addField('?raid','give raids info');
-        embed.addField('?hheeggeell','pls dont use');
-        embed.addField('?craft [item name] [amount]','ex: ?craft soshukatana 1')
+        embed.addField('>raid','give raids info');
+        embed.addField('>hheeggeell','pls dont use');
+        embed.addField('>craft [item name] [amount]','ex: >craft soshukatana 1')
         message.channel.send(embed);
     }
 }
