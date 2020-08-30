b78496094739d0b8d1ff168c004f87c48fa66083
dubietbay <fdadwd@gmail.com>
2020-08-30 12:25:28 +0700

    E

--- commands/lookfor.js
+++ commands/lookfor.js
@@ -7,16 +7,15 @@ module.exports = {
     execute(message, args){ 
         const itemin = args[0].toLowerCase();
         const locations = [
-            {Name: 'kyoto', ID: '561872248'},
-            {Name: 'kansai', ID: '561872248'},
-            {Name: 'chubu', ID: '554670851'},
-            {Name: 'tohoku', ID: '903142964'},
-            {Name: 'kanto', ID: '1469503587'},
-            {Name: 'chugoku', ID: '2329809976'},          
-            {Name: 'mura', ID: '2763010111'},
-            {Name: 'kyushu', ID: '1251162439'},
-            {Name: 'shikoku', ID: '4620197176'},
-            {Name: 'menu', ID: '554664625'},
+            {Name: 'Kansai/Kyoto', ID: '561872248'},
+            {Name: 'Chubu', ID: '554670851'},
+            {Name: 'Tohoku', ID: '903142964'},
+            {Name: 'Kanto', ID: '1469503587'},
+            {Name: 'Chugoku', ID: '2329809976'},          
+            {Name: 'Mura', ID: '2763010111'},
+            {Name: 'Kyushu', ID: '1251162439'},
+            {Name: 'Shikoku', ID: '4620197176'},
+            {Name: 'Menu', ID: '554664625'},
         ];
         const embed = new Discord.MessageEmbed();
         embed.setTitle(`User: ${args[0]}`);
