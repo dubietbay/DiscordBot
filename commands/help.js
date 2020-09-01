e82606d8e94dc1f8ee7de743bb2ea4f47c1cc2d7
dubietbay <fdadwd@gmail.com>
2020-09-01 22:24:26 +0700

    E

--- commands/help.js
+++ commands/help.js
@@ -14,7 +14,7 @@ module.exports = {
         embed.addField('>lookfor [player name]',`gib player's location if playing lotrs`)
         embed.addField('>craft [item name] [amount]','ex: >craft soshukatana 1')
         embed.addField('>info [Equipment]', 'gib equipment info (no weapon yet)')
-        embed.addField('Bring timmy home (to ur server)','[click](https://discord.com/oauth2/authorize?client_id=742196390683345014&scope=bot&permissions=318528)')
+        embed.addField('Bring timmy home (to ur server)','[click](https://discord.com/oauth2/authorize?client_id=750371529015165009&scope=bot&permissions=326720)')
         embed.addField('need help ?','[my house](https://discord.gg/fB5UUjN)')
         message.channel.send(embed);
     }
--- main.js
+++ main.js
@@ -83,85 +83,17 @@ client.on('message',async message => {
         client.commands.get('info').execute(message, args)        
     }
 
+    else if (command === 'joblist') {
+        client.commands.get('job').execute(message, args)
+    }
+
     else if (command === 'servers') {
-            var serverArray = client.guilds
-                for(i = 0; i < serverArray.length; i++) {
-                    serverArray[i].fetchInvites().then(invites => {
-                    invites.map(invite => {
-                    message.channel.send("Invite link: " + invite.url);
-                });
-            });
-        }       
+        client.guilds.cache.forEach((guild) => {
+            message.channel.send(
+              `${guild.name} has a total of ${guild.memberCount} members`
+            )
+          })
     }
-
-    else if (command === 'addtag') {
-        const tagName = args[0]
-        const tagDescription = args[1]
-
-        try {
-        	// equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
-        	const tag = await Tags.create({
-        		name: tagName,
-        		description: tagDescription,
-        		username: message.author.username,
-        	});
-        	return message.reply(`Tag ${tag.name} added.`);
-        }
-        catch (e) {
-        	if (e.name === 'SequelizeUniqueConstraintError') {
-        		return message.reply('That tag already exists.');
-        	}
-        	return message.reply('Something went wrong with adding a tag.');
-        }
-    } 
-    else if (command === 'tag') {
-        const tagName = args[0];
-
-        // equivalent to: SELECT * FROM tags WHERE name = 'tagName' LIMIT 1;
-        const tag = await Tags.findOne({ where: { name: tagName } });
-        if (tag) {
-        	// equivalent to: UPDATE tags SET usage_count = usage_count + 1 WHERE name = 'tagName';
-        	tag.increment('usage_count');
-        	return message.channel.send(tag.get('description'));
-        }
-        return message.reply(`Could not find tag: ${tagName}`);
-    } 
-    else if (command === 'edittag') {
-        const tagName = args[0];
-        const tagDescription = args[1];
-
-        // equivalent to: UPDATE tags (descrption) values (?) WHERE name='?';
-        const affectedRows = await Tags.update({ description: tagDescription }, { where: { name: tagName } });
-        if (affectedRows > 0) {
-        	return message.reply(`Tag ${tagName} was edited.`);
-        }
-        return message.reply(`Could not find a tag with name ${tagName}.`);
-    } 
-    else if (command === 'taginfo') {
-        const tagName = args[0];
-
-        // equivalent to: SELECT * FROM tags WHERE name = 'tagName' LIMIT 1;
-        const tag = await Tags.findOne({ where: { name: tagName } });
-        if (tag) {
-            return message.channel.send(`${tagName} was created by ${tag.username} at ${tag.createdAt} and has been used ${tag.usage_count} times.`);
-        }
-        return message.reply(`Could not find tag: ${tagName}`);
-    } 
-    else if (command === 'showtags') {
-        // equivalent to: SELECT name FROM tags;
-        const tagList = await Tags.findAll({ attributes: ['name'] });
-        const tagString = tagList.map(t => t.name).join(', ') || 'No tags set.';
-        return message.channel.send(`List of tags: ${tagString}`);
-    } 
-    else if (command === 'removetag') {
-        const tagName = args[0];
-        // equivalent to: DELETE from tags WHERE name = ?;
-        const rowCount = await Tags.destroy({ where: { name: tagName } });
-        if (!rowCount) return message.reply('That tag did not exist.');
-
-        return message.reply('Tag deleted.');
-    }
-
 });
 
 client.login(process.env.token);
