43986baf1e154adcc25908b7ba4302ef70e3601b
dubietbay <fdadwd@gmail.com>
2020-09-02 18:42:54 +0700

    E

--- main.js
+++ main.js
@@ -1,37 +1,30 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { Pool } = require('pg');
-const express = require('express')
-const app = express()
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
-
 const prefix = '>';
 const fs = require('fs');
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
+client.connect();
+
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command)
 }
-
-app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
     }
-  })
+    client.end();
+  });
 
 
 client.once('ready', () => {
