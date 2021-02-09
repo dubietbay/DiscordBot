73ce9248bfc072a35fa4766de534cba2c93456ef
dubietbay <fdadwd@gmail.com>
2021-02-09 16:20:05 +0700

    E

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 const noblox = require("noblox.js");
-
+const fetch = require('node-fetch');
 noblox.setCookie(process.env.COOKIE).then(function() {
     console.log("Logged in!")
 }).catch(function(err) {
