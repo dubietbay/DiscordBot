c629ae37e0e2321388bbe099a4bb938dd9f8a1f3
dubietbay <fdadwd@gmail.com>
2021-02-10 15:39:15 +0700

    E

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ module.exports = {
         embed.setThumbnail('https://t1.rbxcdn.com/1194a83cefa36aae9055f96b0165858e');
         embed.addField('>raid','give raids info');
         embed.addField('>lookat [region name]','gib region info')
-        //embed.addField('>lookfor [player name]',`gib player's location if playing lotrs`)
+        embed.addField('>lookfor [player name]',`gib player's location if playing lotrs and not in kuni or dojo`)
         embed.addField('>craft [item name] [amount]','ex: >craft soshukatana 1')
         //embed.addField('>info [Equipment]', 'gib equipment info (no weapon yet) (delayed)')
         embed.addField('Bring timmy home (to ur server)','[click](https://discord.com/oauth2/authorize?client_id=750371529015165009&scope=bot&permissions=326720)')
