f9843247890009896151006c0fd443d2b600a6e3
dubietbay <fdadwd@gmail.com>
2021-02-10 08:06:47 +0700

    E

--- commands/lookat.js
+++ commands/lookat.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
-const nbx = require("noblox.js");
-const nbx = require("noblox.js");
+const noblox = require("noblox.js");
 const Jimp = require('jimp');
 module.exports = {
     name: 'lookat',
@@ -50,7 +49,7 @@ module.exports = {
         function getserverinfo() {
             locations.forEach(el => {
                 if (el.Name == itemin) {
-                    await noblox.http(`https://www.roblox.com/games/getgameinstancesjson?placeId=${el.ID}&startIndex=${args[1]}`, { 
+                    await noblox.http(`https://www.roblox.com/games/getgameinstancesjson?placeId=${el.ID}&startIndex=${args[1]-1}`, { 
                         method: "GET",
                         headers: {
                             cookie: `.ROBLOSECURITY=${process.env.COOKIE}`
