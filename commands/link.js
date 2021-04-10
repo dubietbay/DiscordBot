ac74c029d5cd0a16f2ca1cd93e1c7a37a0db97f1
dubietbay <fdadwd@gmail.com>
2021-04-10 14:16:24 +0700

    E

--- commands/link.js
+++ commands/link.js
@@ -4,14 +4,15 @@ module.exports = {
     name: 'link',
     description: "this is a link command!",
     execute(message, args){
-        let data = {ID: message.channel.id}
+        let data = {IDs: message.channel.id}
         const uri = "mongodb+srv://dubietbay:39611500DUDU@cluster0.vpqpg.mongodb.net/LinkedServers?retryWrites=true&w=majority";
         const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
             client.connect(err => {
             if (err) throw err;
             const collection = client.db("LinkedServers").collection("server");
+            const query = {Name:"CrowdCheck"}
             collection.updateOne(
-                {_id:ObjectId("6071496d7ffadf0004c536f3")},
+                query,
                 { $push: data },
             )
             .then(e => {
