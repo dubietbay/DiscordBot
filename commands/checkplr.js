7997637f00337cca33e48a1a61c7af289161f8f7
dubietbay <fdadwd@gmail.com>
2021-04-10 20:15:47 +0700

    E

--- commands/checkplr.js
+++ commands/checkplr.js
@@ -39,7 +39,7 @@ module.exports = {
                         var pee = JSON.parse(re)
                             pee.Collection[0].CurrentPlayers.forEach(async (plr) => {
                                 if(plr.Thumbnail.Url == avatar) {
-                                    Dclient.channels.cache.get("775352026862977031").send(`player ${getnamefromid(id)} found in ${e.Name} at server ${a+1}.`)
+                                    Dclient.channels.cache.get("775352026862977031").send(`player ${await getnamefromid(id)} found in ${e.Name} at server ${a+1}.`)
                                 }
                             })
                             a = a + 1
