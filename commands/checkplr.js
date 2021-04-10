9a292d1be2050f4ada7e36c46b77851501430dd1
dubietbay <fdadwd@gmail.com>
2021-04-10 19:56:32 +0700

    E

--- commands/checkplr.js
+++ commands/checkplr.js
@@ -62,7 +62,7 @@ module.exports = {
         id.forEach(async (e)=>{
             await fetch(`https://api.roblox.com/users/${e}/onlinestatus/`)
             .then((result) => {
-                console.log(result)
+                   console.log(JSON.parse(result).IsOnline)
             }).catch((err) => {
                 console.log(err)
             });
