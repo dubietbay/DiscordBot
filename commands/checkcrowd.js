b7afa6bb3d9123b4325d9362e3ca1f3e384f4cd4
dubietbay <fdadwd@gmail.com>
2021-04-10 11:29:17 +0700

    E

--- commands/checkcrowd.js
+++ commands/checkcrowd.js
@@ -22,37 +22,37 @@ module.exports = {
             {Name: 'Shikoku', ID: '4620197176'},
         ];
 
-        async function one() {
-            locations.forEach(async (el) =>  {
-                await fetch(`https://games.roblox.com/v1/games/${el.ID}/servers/Public?limit=100&sortOrder=Asc`)
-                    .then(r => {
-                        if(!r.ok) throw 'Invalid response!';
-                        return r.json()
-                    })
-                    .then(e => {
-                        if (e.data.length >= 1){
-                            e.data.forEach(server => {
-                                let servernumber =  e.data.indexOf(server) + 1
-                                let PlrCount = server.playing 
-                                let Ping = server.ping
-                                let Place = el.Name
-                                if (PlrCount >= 5){
-                                    var object = {Head: `Server ${servernumber} in ${Place}`, Tail: `Players count: ${PlrCount} \n Average player's ping: ${Ping}`}
-                                    info.push(object)
-                                }
-                            });
-                        }   
-                    }).catch(er => console.error)
-            });
-        };
-        async function two() {
-            await one()
+        function two() {
             info.forEach(item => {
                 embed.addField(item.Head, item.Tail)
             })
             console.log (info)
             message.channel.send(embed)
         }
-        two()
+
+        locations.forEach(async (el) =>  {
+            await fetch(`https://games.roblox.com/v1/games/${el.ID}/servers/Public?limit=100&sortOrder=Asc`)
+                .then(r => {
+                    if(!r.ok) throw 'Invalid response!';
+                    return r.json()
+                })
+                .then(e => {
+                    if (e.data.length >= 1){
+                        e.data.forEach(server => {
+                            let servernumber =  e.data.indexOf(server) + 1
+                            let PlrCount = server.playing 
+                            let Ping = server.ping
+                            let Place = el.Name
+                            if (PlrCount >= 5){
+                                var object = {Head: `Server ${servernumber} in ${Place}`, Tail: `Players count: ${PlrCount} \n Average player's ping: ${Ping}`}
+                                info.push(object)
+                            }
+                        });
+                    }   
+                }).catch(er => console.error)
+            if(el.Name == "Shikoku"){
+                two
+            }
+        });
     }
 }
