ddc6faf52808d31333efd11c378e9fc4f7c9ddfa
dubietbay <fdadwd@gmail.com>
2021-04-10 13:28:35 +0700

    E

--- commands/link.js
+++ commands/link.js
@@ -1,13 +1,18 @@
 const Discord = require('discord.js');
-const fs = require('fs') 
+const MongoClient = require('mongodb').MongoClient;
 module.exports = {
     name: 'link',
     description: "this is a link command!",
     execute(message, args){
         let data = message.channel.id
-        fs.writeFile('./temp/servers.txt', data, (err) => { 
-            if (err) throw err; 
-        }) 
+        const uri = "mongodb+srv://dubietbay:39611500DUDU@cluster0.vpqpg.mongodb.net/LinkedServers?retryWrites=true&w=majority";
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+            client.connect(err => {
+            if (err) throw err;
+            const collection = client.db("LinkedServers").collection("server");
+            collection.insertOne(data)
+            client.close();
+        });
         message.reply("Linked!")
     }
 }
