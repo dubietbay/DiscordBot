1d175a786e48cca4e58a3e48c39331ed1339255c
dubietbay <fdadwd@gmail.com>
2021-04-10 10:10:48 +0700

    E

--- commands/checkcrowd.js
+++ commands/checkcrowd.js
@@ -5,6 +5,11 @@ module.exports = {
     description: "this is a check command!",
     execute(message, args){
         const embed = new Discord.MessageEmbed();
+        embed.setTitle(`BREAKING NEWS!`);
+        embed.setColor('#f4c871');
+        embed.setAuthor('made by Dub', 'https://i.imgur.com/Rn9muMO.png', 'https://www.roblox.com/users/93839005/profile');
+        embed.setThumbnail('https://t1.rbxcdn.com/1194a83cefa36aae9055f96b0165858e');
+        embed.setTimestamp()
         const locations = [
             {Name: 'Kansai', ID: '561872248'},
             {Name: 'Chubu', ID: '554670851'},
@@ -24,24 +29,19 @@ module.exports = {
                 })
                 .then(e => {
                     if (e.data.length >= 1){
-                        embed.setTitle(`BREAKING NEWS!`);
-                        embed.setColor('#f4c871');
-                        embed.setAuthor('made by Dub', 'https://i.imgur.com/Rn9muMO.png', 'https://www.roblox.com/users/93839005/profile');
-                        embed.setThumbnail('https://t1.rbxcdn.com/1194a83cefa36aae9055f96b0165858e');
-                        embed.setTimestamp()
-                        message.channel.send("why")
                         e.data.forEach(server => {
                             let servernumber =  e.data.indexOf(server) + 1
                             let PlrCount = server.playing 
                             let Ping = server.ping
                             let Place = el.Name
                             if (PlrCount >= 15){
-                                message.channel.send(`Server ${servernumber} in ${Place} is having ${PlrCount} Players \n with average player's ping: ${Ping}`)
-
+                                embed.addField(`Server ${servernumber} in ${Place}`,`Players count: ${PlrCount} \n Average player's ping: ${Ping}`)
                             }
                         });
                     }   
                 }).catch(er => console.error)
         });
+
+        message.channel.send(embed)
     }
 }
