b60fc3d87f3c469998709fe24c0db5c822d5b60e
dubietbay <fdadwd@gmail.com>
2022-04-04 22:56:04 +0700

    e

--- Structures/index.js
+++ Structures/index.js
@@ -1,5 +1,5 @@
 const { Client, Intents, Collection } = require("discord.js");
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: 0 });
 const { promisify } = require("util");
 const { glob } = require("glob");
 const PG = promisify(glob);
@@ -8,8 +8,8 @@ require("dotenv").config();
 
 client.commands = new Collection();
 
+
 ["Events", "Commands"].forEach(handler => {
     require(`./Handlers/${handler}`)(client, PG, Ascii);
 });
-
 client.login(process.env.TOKEN);
